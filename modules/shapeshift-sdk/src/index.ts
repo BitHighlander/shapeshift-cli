d04f68cf30765b41976fdfa8ab21b0d2003fa1e7
highlander <bithighlander@gmail.com>
2021-12-07 17:50:05 -0700

    begin sdk

---
+++ modules/shapeshift-sdk/src/index.ts
@@ -0,0 +1,238 @@
+/*
+
+    ShapeShift Wallet Class
+
+    Class based wallet development
+
+ */
+
+const TAG = " | shapeshift SDK | "
+
+//import chain-adapters
+import { NativeAdapterArgs, NativeHDWallet } from '@shapeshiftoss/hdwallet-native'
+import { BIP32Params, ChainTypes, UtxoAccountType } from '@shapeshiftoss/types'
+import dotenv from 'dotenv'
+dotenv.config()
+import { ChainAdapterManager } from '@shapeshiftoss/chain-adapters'
+
+//requires
+const log = require("@pioneer-platform/loggerdog")()
+let {
+    getPaths,
+    baseAmountToNative,
+    nativeToBaseAmount,
+} = require("@pioneer-platform/pioneer-coins")
+
+export enum HDWALLETS {
+    'native',
+    'keepkey',
+    'trezor',
+    'ledger',
+    'metamask'
+}
+
+export interface Config {
+    username: string;
+    queryKey: string;
+    spec:string,
+    mnemonic: string,
+    unchainedUrls:{
+        [ChainTypes.Bitcoin]: {
+            httpUrl: string,
+            wsUrl: string
+        },
+        [ChainTypes.Ethereum]: {
+            httpUrl: string,
+            wsUrl: string
+        }
+    }
+}
+
+export interface Transaction {
+    blockchain: string;
+    network: string;
+    asset: string;
+    symbol?: string;
+    addressFrom?: string;
+    addressTo: string;
+    amount: string;
+    memo?: string | undefined;
+    nonce?:number
+    feeLevel?:string,
+    fee?:any
+    noBroadcast?:boolean,
+    serialized?:string,
+    unsignedTx?:any
+}
+
+module.exports = class wallet {
+    private type: any;
+    private spec: any;
+    private username: string;
+    private mnemonic: any;
+    private HDWallet: any;
+    private adapters: any;
+    private init: (wallet?: any) => Promise<any>;
+    private unchainedUrls: any;
+    private broadcastTx: (transaction: any) => Promise<any>;
+    private signTx: (transaction: any) => Promise<any>;
+    private buildTx: (transaction: any) => Promise<any>;
+    private queryKey: string;
+    private blockchains: string[];
+    private chainAdapterManager: any;
+    constructor(type:HDWALLETS,config:Config,isTestnet?:boolean) {
+        if(!config.username) throw Error("invalid config missing username")
+        if(!config.queryKey) throw Error("invalid config missing queryKey")
+        if(!config.spec) throw Error("invalid config missing spec")
+        if(!config.mnemonic) throw Error("invalid config missing mnemonic")
+        this.adapters = {}
+        this.blockchains = []
+        this.username = config.username
+        this.queryKey = config.queryKey
+        this.type = type
+        this.spec = config.spec
+        this.mnemonic = config.mnemonic
+        this.unchainedUrls = config.unchainedUrls
+        this.init = async function (wallet?: any) {
+            let tag = TAG + " | init_wallet | "
+            try {
+                //TODO validate mnemonic
+
+                //HDWallet
+                const nativeAdapterArgs: NativeAdapterArgs = {
+                    mnemonic: this.mnemonic,
+                    deviceId: this.username
+                }
+                this.HDWallet = new NativeHDWallet(nativeAdapterArgs)
+                await this.HDWallet.initialize()
+
+                //blockchains
+                this.blockchains = Object.keys(config.unchainedUrls)
+                this.chainAdapterManager = new ChainAdapterManager(config.unchainedUrls)
+
+                for(let i = 0; i < this.blockchains.length; i++){
+                    let blockchain = this.blockchains[i]
+                    log.info(tag,"blockchain: ",blockchain)
+                    //get adapter
+                    this.adapters[blockchain] = this.chainAdapterManager.byChain(blockchain)
+                    //TODO sub to payments
+
+                }
+                return {
+                    blockchains:this.blockchains,
+                    // pubkeys,
+                    // wallet,
+                }
+            } catch (e) {
+                log.error(tag, e)
+                throw e
+            }
+        }
+        this.buildTx = async function (transaction: Transaction) {
+            let tag = TAG + " | buildTx | "
+            try {
+                if(!transaction.blockchain) throw Error("invalid transaction")
+                if(!transaction.addressTo) throw Error("invalid transaction")
+
+                let unsignedTx:any = {}
+
+                switch(transaction.blockchain) {
+                    case ChainTypes.Bitcoin:
+                        //TODO get from this.paths
+                        const btcBip32Params: BIP32Params = {
+                            purpose: 84,
+                            coinType: 0,
+                            accountNumber: 0,
+                            isChange: false,
+                            index: 10
+                        }
+
+                        const txInput = {
+                            to: transaction.addressTo,
+                            value: nativeToBaseAmount('BTC',transaction.amount),
+                            wallet:this.HDWallet,
+                            bip32Params: btcBip32Params,
+                            chainSpecific: { accountType: UtxoAccountType.SegwitNative, satoshiPerByte: '4' }
+                        }
+                        unsignedTx = await this.adapters[ChainTypes.Bitcoin].buildSendTransaction(txInput)
+                        log.info(tag,"unsignedTx: ",unsignedTx)
+                        break;
+                    case ChainTypes.Ethereum:
+                        const ethBip32Params: BIP32Params = { purpose: 44, coinType: 60, accountNumber: 0 }
+                        unsignedTx = await this.adapters[ChainTypes.Bitcoin].buildSendTransaction({
+                            to: transaction.addressTo,
+                            value: nativeToBaseAmount('ETH',transaction.amount),
+                            wallet,
+                            bip32Params: ethBip32Params,
+                            chainSpecific: { gasPrice: '0', gasLimit: '0' }
+                        })
+                        log.info(tag,"unsignedTx: ",unsignedTx)
+                        break;
+                    default:
+                        throw Error("Blockchain not supported! "+transaction.blockchain)
+                    // code block
+                }
+
+                let output = {
+                    transaction,
+                    unsignedTx
+                }
+
+                return output
+            } catch (e) {
+                log.error(tag, e)
+                throw e
+            }
+        }
+        this.signTx = async function (transaction: Transaction) {
+            let tag = TAG + " | signTx | "
+            try {
+                if(!transaction.blockchain) throw Error("invalid transaction missing blockchain")
+                if(!transaction.unsignedTx) throw Error("invalid transaction missing unsignedTx")
+
+                let signedTx:any = await this.adapters[ChainTypes.Bitcoin].signTransaction({
+                    wallet:this.HDWallet,
+                    txToSign: transaction.unsignedTx
+                })
+                log.info(tag,"signedTx: ",signedTx)
+
+                let output = {
+                    transaction,
+                    unsignedTx:transaction.unsignedTx,
+                    serialized:signedTx
+                }
+
+                return output
+            } catch (e) {
+                log.error(tag, e)
+                throw e
+            }
+        }
+        this.broadcastTx = async function (transaction: Transaction) {
+            let tag = TAG + " | broadcastTx | "
+            try {
+                if(!transaction.blockchain) throw Error("invalid transaction missing blockchain")
+                if(!transaction.unsignedTx) throw Error("invalid transaction missing unsignedTx")
+                if(!transaction.serialized) throw Error("invalid transaction missing unsignedTx")
+                if(!this.adapters[transaction.blockchain]) throw Error("blockchain not initialized! "+transaction.blockchain)
+
+                let broadcast:any = await this.adapters[ChainTypes.Bitcoin].broadcastTransaction(transaction.serialized)
+                log.info(tag,"broadcast: ",broadcast)
+
+                let output = {
+                    transaction,
+                    unsignedTx:transaction.unsignedTx,
+                    serialized:transaction.serialized,
+                    broadcast
+                }
+
+                return output
+                return {}
+            } catch (e) {
+                log.error(tag, e)
+                throw e
+            }
+        }
+    }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ require("dotenv").config({path: "../../../.env"});
 require("dotenv").config({path: "../../../../.env"});
 
 
-const TAG = " | App | ";
+const TAG = " | Cli index.js | ";
 //cli tools
 const inquirer = require("inquirer");
 
@@ -23,168 +23,325 @@ import {
     showWelcome
 } from './modules/views'
 
-//platform
-// let platform = require("@pioneer-platform/platform");
-
-//pioneer api
-// let server = require("@pioneer-platform/pioneer-rest-ts")
-// server.start()
-
+//import chain-adapters
+import { NativeAdapterArgs, NativeHDWallet } from '@shapeshiftoss/hdwallet-native'
+import { BIP32Params, ChainTypes, UtxoAccountType } from '@shapeshiftoss/types'
+import dotenv from 'dotenv'
+dotenv.config()
+import { ChainAdapterManager } from '@shapeshiftoss/chain-adapters'
+//requires
 //Subcommand patch
 const program = require( './modules/commander-patch' );
 const log = require("loggerdog-client")();
 
-// must be before .parse()
-program.on('--help', () => {
-    showWelcome()
-});
+const foxContractAddress = '0xc770eefad204b5180df6a14ee197d99d808ee52d'
 
-/*
-    Platform APPs
-        App ecosystem
-        Create
-        Publish
-        Revoke
- */
+//import cliish thing from chain-adapters
+const getWallet = async (): Promise<NativeHDWallet> => {
+    const nativeAdapterArgs: NativeAdapterArgs = {
+        mnemonic: process.env.CLI_MNEMONIC,
+        deviceId: 'test'
+    }
+    const wallet = new NativeHDWallet(nativeAdapterArgs)
+    await wallet.initialize()
 
-const walletCommand = program
-    .command( 'wallet' )
-    .description( 'Create a wallet' )
-    .forwardSubcommands();
+    return wallet
+}
 
-walletCommand
-    .command( 'create' )
-    .action( () => {
-        log.debug(" Create a new wallet")
-    } );
-
-
-/*
-    Pioneer Server
-        (docs)
- */
-//TODO flag run on port
-// const projectCommand = program
-//     .command( 'server start' )
-//     .description( 'Start The Pioneer Server' )
-//     .action( () => {
-//         server.start()
-//     } );
-//
-// projectCommand
-//     .command( 'start' )
-//     .action( () => {
-//         server.start()
-//     } );
-
-//TODO stop server
-//TODO kill on exit
-
-/*
-    Platform Users
-        List users
-        ping user
-        send request
-        view requests
- */
-const userCommand = program
-    .command( 'user' )
-    .description( 'Create a Pioneer Application' )
-    .forwardSubcommands();
-
-userCommand
-    .command( 'list' )
-    .action( () => {
-        log.debug(" user list command passed")
-    } );
-
-userCommand
-    .command( 'request' )
-    .action( () => {
-        log.debug(" user request command passed")
-    } );
-
-/*
-    Platform APPs
-        App ecosystem
-        Create
-        Publish
-        Revoke
- */
-
-const appCommand = program
-    .command( 'app' )
-    .description( 'Create a Pioneer Application' )
-    .forwardSubcommands();
-
-appCommand
-    .command( 'create' )
-    .action( () => {
-        let tag = " | app | "
-
-        const questions = [
-            {
-                type: "input",
-                name: "appname",
-                message: "select an application name",
-                default: "sample app",
-            },
-            //wallets
-        ];
-
-        inquirer.prompt(questions).then(async function (answers: any) {
-            //check if name available
-            log.debug(tag,"answers: ",answers)
-            //generate template to file
-            //platform.create(answers.appname)
-            //create app remote
-
-        });
-    } );
-
-// appCommand
-//     .command( 'ls' )
-//     .action( () => {
-//         //list all apps
-//     } );
-//
-// appCommand
-//     .command( 'publish' )
-//     .action( () => {
-//         let tag = " | app | "
-//         //push to app store
-//     } );
-
-
-/*
-    onStart
-        If no commands, assume --it
- */
-log.debug("args",process.argv)
-
-const onInteractiveTerminal = async function(){
-    let tag = TAG + " | onInteractiveTerminal | "
-    try{
-        log.debug("Starting Interactive Terminal")
-        //start --it mode
-        showWelcome()
-
-        //TODO
-        //all the things
-
-
-    }catch(e){
-        log.error("Terminal Exit: ",e)
-        process.exit(2)
+const unchainedUrls = {
+    [ChainTypes.Bitcoin]: {
+        httpUrl: 'https://dev-api.bitcoin.shapeshift.com',
+        wsUrl: 'wss://dev-api.bitcoin.shapeshift.com'
+    },
+    [ChainTypes.Ethereum]: {
+        httpUrl: 'https://dev-api.ethereum.shapeshift.com',
+        wsUrl: 'wss://dev-api.ethereum.shapeshift.com'
     }
 }
 
-if(process.argv.length === 2){
-    onInteractiveTerminal()
-} else {
-    program.parse( process.argv );
+const main = async () => {
+    try {
+        const chainAdapterManager = new ChainAdapterManager(unchainedUrls)
+        const wallet = await getWallet()
+
+        /** BITCOIN CLI */
+        // const btcChainAdapter = chainAdapterManager.byChain(ChainTypes.Bitcoin)
+        // const btcBip32Params: BIP32Params = {
+        //     purpose: 84,
+        //     coinType: 0,
+        //     accountNumber: 0,
+        //     isChange: false,
+        //     index: 10
+        // }
+        //
+        // const btcAddress = await btcChainAdapter.getAddress({
+        //     wallet,
+        //     bip32Params: btcBip32Params,
+        //     accountType: UtxoAccountType.SegwitNative
+        // })
+        // console.log('btcAddress:', btcAddress)
+        //
+        // const btcAccount = await btcChainAdapter.getAccount(btcAddress)
+        // console.log('btcAccount:', btcAccount)
+        //
+        // await btcChainAdapter.subscribeTxs(
+        //     { wallet, bip32Params: btcBip32Params, accountType: UtxoAccountType.SegwitNative },
+        //     (msg) => console.log(msg),
+        //     (err) => console.log(err)
+        // )
+        //
+        // const txInput = {
+        //     to: 'bc1qkumt74m9tl0anhrga37t52xlknu0eag3scg7nr',
+        //     value: '400',
+        //     wallet,
+        //     bip32Params: btcBip32Params,
+        //     chainSpecific: { accountType: UtxoAccountType.SegwitNative, satoshiPerByte: '4' }
+        // }
+        //
+        // try {
+        //     const btcUnsignedTx = await btcChainAdapter.buildSendTransaction(txInput)
+        //     const btcSignedTx = await btcChainAdapter.signTransaction({
+        //         wallet,
+        //         txToSign: btcUnsignedTx.txToSign
+        //     })
+        //     console.log('btcSignedTx:', btcSignedTx)
+        // } catch (err) {
+        //     console.log('btcTx error:', err.message)
+        // }
+        //
+        // // // const btcTxID = await btcChainAdapter.broadcastTransaction(btcSignedTx)
+        // // // console.log('btcTxID: ', txid)
+
+        // /** ETHEREUM CLI */
+        const ethChainAdapter = chainAdapterManager.byChain(ChainTypes.Ethereum)
+        const ethBip32Params: BIP32Params = { purpose: 44, coinType: 60, accountNumber: 0 }
+
+        const ethAddress = await ethChainAdapter.getAddress({ wallet, bip32Params: ethBip32Params })
+        console.log('ethAddress:', ethAddress)
+
+        const ethAccount = await ethChainAdapter.getAccount(ethAddress)
+        console.log('ethAccount:', ethAccount)
+
+        await ethChainAdapter.subscribeTxs(
+            { wallet, bip32Params: ethBip32Params },
+            (msg) => console.log(msg),
+            (err) => console.log(err)
+        )
+
+        // send eth example
+        try {
+            const ethUnsignedTx = await ethChainAdapter.buildSendTransaction({
+                to: `0x47CB53752e5dc0A972440dA127DCA9FBA6C2Ab6F`,
+                value: '1',
+                wallet,
+                bip32Params: ethBip32Params,
+                chainSpecific: { gasPrice: '0', gasLimit: '0' }
+            })
+            const ethSignedTx = await ethChainAdapter.signTransaction({
+                wallet,
+                txToSign: ethUnsignedTx.txToSign
+            })
+            console.log('ethSignedTx:', ethSignedTx)
+        } catch (err) {
+            console.log('ethTx error:', err.message)
+        }
+
+        // const ethTxID = await ethChainAdapter.broadcastTransaction(ethSignedTx)
+        // console.log('ethTxID:', ethTxID)
+
+        // send fox example (erc20)
+        try {
+            const erc20UnsignedTx = await ethChainAdapter.buildSendTransaction({
+                to: `0x47CB53752e5dc0A972440dA127DCA9FBA6C2Ab6F`,
+                value: '1',
+                wallet,
+                bip32Params: ethBip32Params,
+                chainSpecific: { gasPrice: '0', gasLimit: '0', erc20ContractAddress: foxContractAddress }
+            })
+            const erc20SignedTx = await ethChainAdapter.signTransaction({
+                wallet,
+                txToSign: erc20UnsignedTx.txToSign
+            })
+            console.log('erc20SignedTx:', erc20SignedTx)
+
+            //const erc20TxID = await ethChainAdapter.broadcastTransaction(erc20SignedTx)
+            //console.log('erc20TxID:', erc20TxID)
+        } catch (err) {
+            console.log('erc20Tx error:', err.message)
+        }
+
+        /** COSMOS CLI */
+
+        /** OSMOSIS CLI */
+
+    } catch (err) {
+        console.error(err)
+    }
 }
 
+main().then(() => console.log('DONE'))
+
+
+//import cliish thing from swapper
+
+//import cliish thing from staker
+
+//TODO generalized TX builder class that wraps all these things
+
+
+
+//TODO make a real cli bro
+// // must be before .parse()
+// program.on('--help', () => {
+//     showWelcome()
+// });
+//
+// /*
+//     Platform APPs
+//         App ecosystem
+//         Create
+//         Publish
+//         Revoke
+//  */
+//
+// const walletCommand = program
+//     .command( 'wallet' )
+//     .description( 'Create a wallet' )
+//     .forwardSubcommands();
+//
+// walletCommand
+//     .command( 'create' )
+//     .action( () => {
+//         log.debug(" Create a new wallet")
+//     } );
+//
+//
+// /*
+//     Pioneer Server
+//         (docs)
+//  */
+// //TODO flag run on port
+// // const projectCommand = program
+// //     .command( 'server start' )
+// //     .description( 'Start The Pioneer Server' )
+// //     .action( () => {
+// //         server.start()
+// //     } );
+// //
+// // projectCommand
+// //     .command( 'start' )
+// //     .action( () => {
+// //         server.start()
+// //     } );
+//
+// //TODO stop server
+// //TODO kill on exit
+//
+// /*
+//     Platform Users
+//         List users
+//         ping user
+//         send request
+//         view requests
+//  */
+// const userCommand = program
+//     .command( 'user' )
+//     .description( 'Create a Pioneer Application' )
+//     .forwardSubcommands();
+//
+// userCommand
+//     .command( 'list' )
+//     .action( () => {
+//         log.debug(" user list command passed")
+//     } );
+//
+// userCommand
+//     .command( 'request' )
+//     .action( () => {
+//         log.debug(" user request command passed")
+//     } );
+//
+// /*
+//     Platform APPs
+//         App ecosystem
+//         Create
+//         Publish
+//         Revoke
+//  */
+//
+// const appCommand = program
+//     .command( 'app' )
+//     .description( 'Create a Pioneer Application' )
+//     .forwardSubcommands();
+//
+// appCommand
+//     .command( 'create' )
+//     .action( () => {
+//         let tag = " | app | "
+//
+//         const questions = [
+//             {
+//                 type: "input",
+//                 name: "appname",
+//                 message: "select an application name",
+//                 default: "sample app",
+//             },
+//             //wallets
+//         ];
+//
+//         inquirer.prompt(questions).then(async function (answers: any) {
+//             //check if name available
+//             log.debug(tag,"answers: ",answers)
+//             //generate template to file
+//             //platform.create(answers.appname)
+//             //create app remote
+//
+//         });
+//     } );
+//
+// // appCommand
+// //     .command( 'ls' )
+// //     .action( () => {
+// //         //list all apps
+// //     } );
+// //
+// // appCommand
+// //     .command( 'publish' )
+// //     .action( () => {
+// //         let tag = " | app | "
+// //         //push to app store
+// //     } );
+//
+//
+// /*
+//     onStart
+//         If no commands, assume --it
+//  */
+// log.debug("args",process.argv)
+//
+// const onInteractiveTerminal = async function(){
+//     let tag = TAG + " | onInteractiveTerminal | "
+//     try{
+//         log.debug("Starting Interactive Terminal")
+//         //start --it mode
+//         showWelcome()
+//
+//         //TODO
+//         //all the things
+//
+//
+//     }catch(e){
+//         log.error("Terminal Exit: ",e)
+//         process.exit(2)
+//     }
+// }
+//
+// if(process.argv.length === 2){
+//     onInteractiveTerminal()
+// } else {
+//     program.parse( process.argv );
+// }
+//
 
 
 
